c49666027337a5d80f0e3f9f3b88e20c4b59b3f3
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-28 17:23:38 +0100

    feat: video player play button ui string

--- packages/starlight-videos/translations.ts
+++ packages/starlight-videos/translations.ts
@@ -1,9 +1,11 @@
 export const Translations = {
   en: {
+    'starlightVideos.video.play': 'Play',
     'starlightVideos.videos.count_one': '{{count}} video',
     'starlightVideos.videos.count_other': '{{count}} videos',
   },
   fr: {
+    'starlightVideos.video.play': 'Lire',
     'starlightVideos.videos.count_one': '{{count}} vidéo',
     'starlightVideos.videos.count_other': '{{count}} vidéos',
   },
