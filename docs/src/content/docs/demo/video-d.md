765a63d4f11010abc07f8ccf83d154d23b000188
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-28 17:58:20 +0100

    feat: format duration

--- docs/src/content/docs/demo/video-d.md
+++ docs/src/content/docs/demo/video-d.md
@@ -3,7 +3,7 @@ title: A AVideo D
 video:
   type: video
   link: https://www.example.com
-  duration: 3000
+  duration: 400
   description: This is a video (not in a collection). lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
   date: 2021-01-02
 ---
--- packages/starlight-videos/translations.ts
+++ packages/starlight-videos/translations.ts
@@ -1,10 +1,18 @@
 export const Translations = {
   en: {
+    'starlightVideos.video.duration': 'Duration',
+    'starlightVideos.video.duration.minutes_one': '{{count}} min',
+    'starlightVideos.video.duration.minutes_other': '{{count}} mins',
+    'starlightVideos.video.duration.hours_one': '{{count}} hr {{minutes}}',
+    'starlightVideos.video.duration.hours_other': '{{count}} hrs {{minutes}}',
     'starlightVideos.video.play': 'Play',
     'starlightVideos.videos.count_one': '{{count}} video',
     'starlightVideos.videos.count_other': '{{count}} videos',
   },
   fr: {
+    'starlightVideos.video.duration': 'Durée',
+    'starlightVideos.video.duration.minutes': '{{count}} min',
+    'starlightVideos.video.duration.hours': '{{count}} h {{minutes}}',
     'starlightVideos.video.play': 'Lire',
     'starlightVideos.videos.count_one': '{{count}} vidéo',
     'starlightVideos.videos.count_other': '{{count}} vidéos',
